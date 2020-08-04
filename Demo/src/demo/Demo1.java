a393094e8cb5166699608396c7403e547a489751
13943 <13943@LAPTOP-R57NP4E8>
2020-08-04 17:17:36 +0800

    练习用的

---
+++ Demo/src/demo/Demo1.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Demo1 {
+public static void main(String[] args) {
+	String st="sdf";
+	st+=100;
+	System.out.println(st);
+	StringBuffer str = new StringBuffer();
+	StringBuffer append = str.append("dsfaffdsfa");
+	System.out.println(append);
+}
+}
